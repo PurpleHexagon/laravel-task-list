bce7967da2e4b27c8f658d64492286015d0c43ae
Jon Day <jon@plantigrate.com>
2020-01-26 12:10:32 +0000

    Add task model

---
+++ app/Task.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Task
+ * @package App
+ */
+class Task extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'title', 'details',
+    ];
+}
