70e40464d182849386a0fb13ef38470dda87ed2f
Jon Day <jon@plantigrate.com>
2020-09-27 13:20:28 +0100

    Housekeeping

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Task;
+use GuzzleHttp\Client;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -18,7 +22,9 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    use AuthenticatesUsers {
+        login as loginParent;
+    }
 
     /**
      * Where to redirect users after login.
@@ -35,5 +41,48 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    /**
+     * @param Request $request
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function login(Request $request)
+    {
+        $LoginResponse = $this->loginParent($request);
+
+        $password = $request->post('password');
+        $hashedPassword = sha1($password);
+        $prefix = substr($hashedPassword, 0 , 5);
+        $suffix = substr($hashedPassword, 5 );
+        $client = new Client();
+
+        $response = $client->request('GET', "https://api.pwnedpasswords.com/range/$prefix");
+
+        $hashSuffixes = explode("\n", $response->getBody()->getContents());
+
+        $hashSuffixesMapped = array_map(
+            function ($item) {
+                return explode(':', $item);
+            },
+            $hashSuffixes
+        );
+
+        foreach ($hashSuffixesMapped as $hashSuffix) {
+            if (strtoupper($hashSuffix[0]) === strtoupper($suffix)) {
+                $user = Auth::user();
+                $task = new Task();
+                $task->fill([
+                    'title' => 'Youve been PWDNED',
+                    'details' => 'You need to change your password'
+                ]);
+
+                $task->user_id = $user->id;
+
+                $task->save();
+            }
+        }
+
+        return $LoginResponse;
     }
 }
